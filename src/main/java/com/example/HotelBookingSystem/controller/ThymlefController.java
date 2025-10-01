707ba60febc3028e8f4306fc2c7dfd714b937cc3
Đỗ Văn Hải <haidv28062004@gmail.com>
2025-10-02 02:11:51 +0700

    tạo thông tin chi tiết phòng

--- src/main/java/com/example/HotelBookingSystem/controller/ThymlefController.java
+++ src/main/java/com/example/HotelBookingSystem/controller/ThymlefController.java
@@ -1,20 +1,32 @@
 package com.example.HotelBookingSystem.controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.example.HotelBookingSystem.repository.CustomerRepository;
+import com.example.HotelBookingSystem.service.RoomService;
+
 @Controller
 public class ThymlefController {
-        @Autowired
+    @Autowired
     private CustomerRepository customerRepository;
 
-        @GetMapping("/")
-        public String index(Model model) {
-            model.addAttribute("customers", customerRepository.findAll());
-            model.addAttribute("message", "Xin chào Thymeleaf!");
-            return "index";  // trỏ tới file templates/index.html
-        }
+    @Autowired
+    private RoomService roomService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("customers", customerRepository.findAll());
+        model.addAttribute("message", "Xin chào Thymeleaf!");
+        return "index"; // trỏ tới file templates/index.html
     }
 
+    @GetMapping("/detail/{id}")
+    public String detail(Model model, @PathVariable Integer id) {
+        model.addAttribute("room", roomService.getRoomDetail(id));
+        return "detail"; // trỏ tới file templates/index.html
+    }
+}
