f6832e089bbc6be97782370a34e41488e9f28c58
ngtrunghieu116 <hieunavy1106@gmail.com>
2025-10-05 19:40:32 +0700

    done fe manage

--- src/main/java/com/example/HotelBookingSystem/service/ManageRoomServiceImpl.java
+++ src/main/java/com/example/HotelBookingSystem/service/ManageRoomServiceImpl.java
@@ -1,13 +1,20 @@
 package com.example.HotelBookingSystem.service;
 
 import com.example.HotelBookingSystem.dto.ManageRoomDTO;
+import com.example.HotelBookingSystem.model.ManageRoom;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface ManageRoomServiceImpl {
     List<ManageRoomDTO> getAll();
     ManageRoomDTO getById(Integer id);
-    ManageRoomDTO create(ManageRoomDTO dto);
+    ManageRoomDTO create(LocalDate startDate, LocalDate endDate, Integer roomId, String note, String status);
     ManageRoomDTO update(Integer id, ManageRoomDTO dto);
     void delete(Integer id);
+    ManageRoomDTO updateStatus(Integer id, ManageRoom.Status status);
+
+    Page<ManageRoomDTO>getAllManageRooms(LocalDate start, LocalDate end, Pageable pageable);
 }
